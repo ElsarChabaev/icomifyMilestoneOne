49b61a4a79d1b263c7e362180ef5b5eebbd0916c
Elsar Chabaev <s0561238@htw-berlin.de>
2022-10-24 18:31:06 +0200

    Init

---
+++ src/main/java/de/webtech/milestone/IcomifyApplication.java
@@ -0,0 +1,13 @@
+package de.webtech.milestone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcomifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcomifyApplication.class, args);
+	}
+
+}
